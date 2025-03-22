abb6940cac85ed96239ec52cb34bec4980cadfd9
Emerson Millard <emillard.code@gmail.com>
2025-03-22 12:00:00 -0700

    Added challenge attempt.

--- src/main/java/com/project/attempt/LeetCodeAttempt.java
+++ src/main/java/com/project/attempt/LeetCodeAttempt.java
@@ -4,6 +4,44 @@ public class LeetCodeAttempt {
 
     public static void main(String[] args) {
 
+        System.out.println(alternatingGroupsII(new int[]{0,1,0,1,0}, 3));
+
+        System.out.println(alternatingGroupsII(new int[]{0,1,0,0,1,0,1}, 6));
+
+        System.out.println(alternatingGroupsII(new int[]{1,1,0,1}, 4));
+
+    }
+
+    public static int alternatingGroupsII(int[] colors, int k) {
+
+        int alternatingGroups = 0;
+
+        int previousColor = -1;
+        int currentColor = -1;
+
+        for (int i = 0; i < colors.length; i++) {
+
+            boolean alternating = true;
+            previousColor = colors[i];
+
+            for (int j = i + 1; j < i + k; j++) {
+
+                int currentIndex = j;
+                if (j >= colors.length) { currentIndex = j - colors.length; }
+
+                currentColor = colors[currentIndex];
+                if (currentColor == previousColor) { alternating = false; }
+
+                previousColor = currentColor;
+
+            }
+
+            if (alternating) { alternatingGroups++; }
+
+        }
+
+        return alternatingGroups;
+
     }
 
 }
